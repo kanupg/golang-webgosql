aad6a0d9d8ac37784b02bde87ff3fb2a183bab1f
kanupg <fujita.kenseib+kanu@gmail.com>
2023-02-14 01:15:23 +0900

    回答

--- functions/exercise-fibonacci-closure.go
+++ functions/exercise-fibonacci-closure.go
@@ -5,6 +5,13 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
+	idxN, idxNPlus1 := 0, 1
+	return func() int {
+		ans := idxN
+		idxN = idxNPlus1
+		idxNPlus1 = idxN + ans
+		return ans
+	}
 }
 
 func main() {
